2d57396a76f6b3d630298e0e96ee5f3ba349bbbb
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2025-12-23 08:33:10 -0400

    feat: adicionando ícone no App

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { colors } from '@/theme/colors';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -10,11 +11,10 @@ export default function App() {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
