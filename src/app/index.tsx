ec2f6dc84b4295399fdabbcfeddab3e918f0d651
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-01-12 15:45:16 -0400

    Componente List

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { View } from "react-native";
+import { List } from "@/components/List";
 import { Target } from "@/components/Target";
 import { HomeHeader } from "@/components/HomeHeader";
 
@@ -7,12 +8,23 @@ const summary = {
     input: { label: "Entradas", value: "R$ 6,184.90" },
     output: { label: "Saídas", value: "-R$ 883.65" }
 }
-const targets = [{ name: "Comprar uma cadeira ergonômica", percentage: "75%", current: "900,00", target: "1.200,00", },]
+const targets = [
+    { id: "1", name: "Apple Watch", percentage: "50%", current: "R$ 580,00", target: "R$ 1.790,00", },
+    { id: "2", name: "Comprar uma cadeira ergonômica", percentage: "75%", current: "R$ 900,00", target: "R$ 1.200,00", },
+    { id: "3", name: "Fazer uma viagem para o Rio de Janeiro", percentage: "75%", current: "R$ 1.200,00", target: "R$ 3.000,00", },
+]
 export default function Index() {
     return (
         <View style={{ flex: 1 }}>
             <HomeHeader data={summary} />
-            <Target data={targets[0]} />
+            <List
+                title="Metas"
+                data={targets}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => <Target data={item} />}
+                emptyMessage="Nenhuma meta. Toque em nova meta para criar."
+                containerStyle={{ paddingHorizontal: 24 }}
+            />
         </View>
     )
 }
---
+++ src/components/List/index.tsx
@@ -0,0 +1,28 @@
+import { colors } from "@/theme"
+import { styles } from "./styles"
+import { Separator } from "../Separator"
+import { FlatList, FlatListProps, StyleProp, Text, View, ViewStyle } from "react-native"
+
+type Props<T> = FlatListProps<T> & {
+    title: string
+    emptyMessage?: string
+    containerStyle?: StyleProp<ViewStyle>
+}
+export function List<T>({ title, emptyMessage, containerStyle, data, renderItem, ...rest }: Props<T>) {
+    return (
+        <View style={[styles.container, containerStyle]}>
+            <Text style={styles.title}>{title}</Text>
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+                ItemSeparatorComponent={() => <Separator color={colors.gray[200]} />}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.listContent}
+                ListEmptyComponent={() => (
+                    <Text style={styles.empty}>{emptyMessage}</Text>
+                )}
+                {...rest}
+            />
+        </View>
+    )
+}
