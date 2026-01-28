34574d3746e20a220fae2783e659f1ce21e0b5a7
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-01-28 09:55:22 -0400

    Criando a interface - Componente Transaction

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,19 +1,39 @@
 import { View } from "react-native";
-import { Progress } from "@/components/Progress";
 import { useLocalSearchParams } from "expo-router";
+
+import { List } from "@/components/List";
+import { Progress } from "@/components/Progress";
 import { PageHeader } from "@/components/PageHeader";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 const details = {
     current: "R$ 580,00",
     target: "R$ 1.790,00",
     percentage: 25
 }
+const transactions: TransactionProps[] = [
+    {
+        id: "1",
+        value: "R$ 20,00",
+        date: "12/04/25",
+        type: TransactionTypes.Output
+    },
+    {
+        id: "1",
+        value: "R$ 300,00",
+        date: "12/04/25",
+        description: "CDB de 110% no banco XPTO",
+        type: TransactionTypes.Input
+    },
+]
 export default function InProgress() {
     const params = useLocalSearchParams<{ id: string }>()
     return (
         <View style={{ flex: 1, padding: 24, gap: 32 }}>
             <PageHeader title="Apple Watch" rightButton={{ icon: "edit", onPress: () => { } }} />
             <Progress data={details} />
+            <List title="Transações" data={transactions} renderItem={({ item }) => <Transaction data={item} onRemove={() => { }} />} />
         </View>
     )
 }
