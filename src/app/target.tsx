c75f7011c2972f3d431fefdd6bcf2ff533e92fd6
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-01-30 08:49:54 -0400

    Criando a interface - Interface de transação

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -13,7 +13,7 @@ export default function Target() {
             />
             <View style={{ marginTop: 32, gap: 24 }}>
                 <Input label="Nome da Meta" placeholder="Ex: Viagem para praia, Apple Watch" />
-                <CurrencyInput label="Valor alvo" value={0} />
+                <CurrencyInput label="Valor alvo (R$)" value={0} />
                 <Button title="Salvar" isProcessing />
             </View>
         </View>
