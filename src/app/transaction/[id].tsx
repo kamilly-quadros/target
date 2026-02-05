984bda7defb595d4c23d6f548f1dbf57895ed515
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-02-05 08:17:33 -0400

    Criando a interface - Componente TransactionType

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import { View } from "react-native";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 import { useLocalSearchParams } from "expo-router"
 import { PageHeader } from "@/components/PageHeader";
 import { CurrencyInput } from "@/components/CurrencyInput";
+import { TransactionTypes } from "@/utils/TransactionTypes";
+import { TransactionType } from "@/components/TransactionType";
 
 export default function Transaction() {
     const params = useLocalSearchParams<{ id: string }>()
+    const [type, setType] = useState(TransactionTypes.Input)
     return (
         <View style={{ flex: 1, padding: 24 }}>
             <PageHeader
@@ -14,6 +18,7 @@ export default function Transaction() {
                 subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
             />
             <View style={{ marginTop: 32, gap: 24 }}>
+                <TransactionType selected={type} onChange={setType} />
                 <CurrencyInput label="Valor (R$)" value={0} />
                 <Input label="Motivo (opcional)" placeholder="Ex: Investir em CDB de 110% no banco XPTO" />
                 <Button title="Salvar" />
