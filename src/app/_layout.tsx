738a698729df1cd2db9a650b4f00f9189b870ce7
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-02-18 08:14:19 -0400

    SQLite - Conexão com o banco de dados

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,20 @@
+import { Suspense } from "react"
 import { Stack } from "expo-router"
 import { colors } from "@/theme/colors"
+import { migrate } from "@/database/migrate"
+import { SQLiteProvider } from "expo-sqlite"
 import { Loading } from "@/components/Loading"
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold } from "@expo-google-fonts/inter"
+
 
 export default function Layouit() {
     const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_500Medium, Inter_700Bold })
     if (!fontsLoaded) { return <Loading /> }
     return (
-        <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: colors.white } }} />
+        <Suspense fallback={<Loading />}>
+            <SQLiteProvider databaseName="target.db" onInit={migrate} useSuspense>
+                <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: colors.white } }} />
+            </SQLiteProvider>
+        </Suspense>
     )
 }
