900288d40c363ee319f15d0493e29a7eb566507a
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-03-24 08:30:06 -0400

    SQLite - exibindo a meta

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,38 +1,55 @@
-import { View } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
+import { Alert, View } from "react-native";
+import { useCallback, useState } from "react";
+import { router, useFocusEffect, useLocalSearchParams } from "expo-router";
 
 import { List } from "@/components/List";
 import { Button } from "@/components/Button";
+import { Loading } from "@/components/Loading";
 import { Progress } from "@/components/Progress";
 import { PageHeader } from "@/components/PageHeader";
-import { Transaction, TransactionProps } from "@/components/Transaction";
-import { TransactionTypes } from "@/utils/TransactionTypes";
+import { Transaction } from "@/components/Transaction";
 
-const details = {
-    current: "R$ 580,00",
-    target: "R$ 1.790,00",
-    percentage: 25
-}
-const transactions: TransactionProps[] = [
-    {
-        id: "1",
-        value: "R$ 20,00",
-        date: "12/04/25",
-        type: TransactionTypes.Output
-    },
-    {
-        id: "1",
-        value: "R$ 300,00",
-        date: "12/04/25",
-        description: "CDB de 110% no banco XPTO",
-        type: TransactionTypes.Input
-    },
-]
+import { numberToCurrency } from "@/utils/numberToCurrency";
+
+import { useTargetDatabase } from "@/database/useTargetDatabase";
+
 export default function InProgress() {
+    const targetDatabase = useTargetDatabase()
+    const [isFetching, setIsFetching] = useState(true)
     const params = useLocalSearchParams<{ id: string }>()
+    const [details, setDetails] = useState({ name: "", current: "R$0,00", target: "R$ 0,00", percentage: 0 })
+    async function fetchData() {
+        const fetchDetailsPromise = fetchDetails()
+        await Promise.all([fetchDetailsPromise])
+        setIsFetching(false)
+    }
+    async function fetchDetails() {
+        try {
+            const response = await targetDatabase.show(Number(params.id))
+            if (!response) {
+                Alert.alert("Erro", "Meta não encontrada.")
+                return
+            }
+            setDetails({
+                name: response.name,
+                current: numberToCurrency(response.current),
+                target: numberToCurrency(response.amount),
+                percentage: response.percentage
+            })
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível carregar os detalhes da meta.")
+            console.error(error)
+        }
+    }
+    useFocusEffect(
+        useCallback(() => {
+            fetchData()
+        }, [])
+    )
+    if (isFetching) { return <Loading /> }
     return (
         <View style={{ flex: 1, padding: 24, gap: 32 }}>
-            <PageHeader title="Apple Watch" rightButton={{ icon: "edit", onPress: () => { } }} />
+            <PageHeader title={details.name} rightButton={{ icon: "edit", onPress: () => { } }} />
             <Progress data={details} />
             <List
                 title="Transações"
