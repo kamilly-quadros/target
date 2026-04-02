01d242c719eacb864b0cedabd767c00b1b8a02a9
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-04-02 08:09:01 -0400

    SQLite - Listando transações da meta

--- src/database/useTransactionsDatabase.ts
+++ src/database/useTransactionsDatabase.ts
@@ -5,6 +5,14 @@ export type TransactionCreate = {
     amount: number
     observation?: string
 }
+export type TransactionResponse = {
+    id: number
+    target_id: number
+    amount: number
+    observation: string
+    created_at: Date
+    updated_at: Date
+}
 export function useTransactionsDatabase() {
     const database = useSQLiteContext()
     async function create(data: TransactionCreate) {
@@ -16,7 +24,15 @@ export function useTransactionsDatabase() {
         `)
         statement.executeAsync({ $target_id: data.target_id, $amount: data.amount, $observation: data.observation ?? null })
     }
+    function listByTargetId(id: number) {
+        return database.getAllAsync<TransactionResponse>(`
+        SELECT id, target_id, amount, observation, created_at, updated_at
+        FROM transactions
+        WHERE target_id = ${id}
+        ORDER BY created_at DESC
+    `)
+    }
     return {
-        create,
+        create, listByTargetId
     }
 }
