94c4f9cfe2a0374ea5e9670c92c30a58122f72df
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-04-13 17:30:42 -0400

    SQLite - Criando o método de summary

--- src/database/useTransactionDatabase.ts
+++ src/database/useTransactionDatabase.ts
@@ -13,6 +13,10 @@ export type TransactionResponse = {
     created_at: Date
     updated_at: Date
 }
+export type Summary = {
+    input: number
+    output: number
+}
 export function useTransactionDatabase() {
     const database = useSQLiteContext()
     async function create(data: TransactionCreate) {
@@ -32,10 +36,18 @@ export function useTransactionDatabase() {
         ORDER BY created_at DESC
     `)
     }
-    async function remove(id:number) {
+    async function remove(id: number) {
         await database.runAsync("DELETE FROM transactions WHERE id = ?", id)
     }
+    function summary() {
+        return database.getFirstAsync<Summary>(`
+            SELECT
+                COALESCE(SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END),0) AS input,
+                COALESCE(SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END),0) AS output
+            FROM transactions
+        `)
+    }
     return {
-        create, listByTargetId, remove
+        create, listByTargetId, remove, summary
     }
 }
