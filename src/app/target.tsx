613b239bb8ccfa46a69f6d50428f74e927d86db7
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2026-04-15 08:16:51 -0400

    SQLite - Ajustando StatusBar

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,4 +1,4 @@
-import { Alert, View } from "react-native";
+import { Alert, StatusBar, View } from "react-native";
 import { Input } from "@/components/Input";
 import { useEffect, useState } from "react";
 import { Button } from "@/components/Button";
@@ -74,6 +74,7 @@ export default function Target() {
     useEffect(() => { if (params.id) { fetchDetails(Number(params.id)) } }, [params.id])
     return (
         <View style={{ flex: 1, padding: 24 }}>
+            <StatusBar barStyle="dark-content" />
             <PageHeader
                 title="Meta"
                 subtitle="Economize para alcançar sua meta financeira."
